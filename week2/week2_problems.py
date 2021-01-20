30cf3e2def4e131ce78d62af8f65bb203595ea75
David Scobie <david.scobie.20@ucl.ac.uk>
2021-01-20 19:07:49 +0000

    part d complete

--- week2/week2_problems.py
+++ week2/week2_problems.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-n=4
+n=100
 x = np.linspace(-1, 1, num=n)
 
 del_n = 2/(n-1)
@@ -21,14 +21,23 @@ plt.xlabel("x")
 plt.ylabel("G(x)")
 plt.plot(x,g)
 
-A=[]
+A_big=[]
 for i in range (n):
     for j in range (n):
-        A.append(start*(np.exp(-((x[i]-x[j])**2)/(2*((std)**2)))))
-print(A)
-#Now need to split list into list of lists to make 2D array
+        A_big.append(start*(np.exp(-((x[i]-x[j])**2)/(2*((std)**2)))))
 
-# Atmp = np.array(np.ceil(A/np.max(A)*256), dtype = np.uint8)
+#Now need to split list into list of lists to make 2D array
+A = [ A_big [i:i + n] for i in range(0, len(A_big), n) ]
+
+
+Atmp = np.array(np.ceil(A/np.max(A)*256), dtype = np.uint8)
+Aimg = cv2.applyColorMap(Atmp, cv2.COLORMAP_JET)
+cv2.imwrite("Aimage3.png",Aimg)
+
+# B = [[1,2,3],[4,5,6]]
+# print(B)
+
+# Atmp = np.array(np.ceil(B/np.max(B)*256), dtype = np.uint8)
 # Aimg = cv2.applyColorMap(Atmp, cv2.COLORMAP_JET)
 # cv2.imwrite("Aimage3.png",Aimg)
 
