660ee219b4d7da5953e440e69eb1caf2933d8133
David Scobie <david.scobie.20@ucl.ac.uk>
2021-01-26 20:09:24 +0000

    have completed part c

--- week3/week3_problems.py
+++ week3/week3_problems.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
+#a
 a=np.zeros((1,5))
 b=np.ones((1,35))
 c=np.hstack((a,b))
@@ -19,5 +21,29 @@ x = np.linspace(-1, 1, num=200)
 plt.figure(0)
 plt.xlabel("x")
 plt.ylabel("f")
-plt.plot(x,f)
+# plt.plot(x,f)
+
+
+#b
+n=200
+del_n=2/n
+std = 0.2
+start = del_n/(((2*np.pi)**0.5)*std)
+A_big=[]
+for i in range (n):
+    for j in range (n):
+        A_big.append(start*(np.exp(-((x[i]-x[j])**2)/(2*((std)**2)))))
+#Now need to split list into list of lists to make 2D array
+A = [ A_big [i:i + n] for i in range(0, len(A_big), n) ]
+
+Atmp = np.array(np.ceil(A/np.max(A)*256), dtype = np.uint8)
+Aimg = cv2.applyColorMap(Atmp, cv2.COLORMAP_JET)
+cv2.imwrite("Aimage3.png",Aimg)
+
+U, W1, VT = np.linalg.svd(A)
+plt.plot(W1)
+
+print(np.var(W1))
+
+con = np.convolve()
 plt.show()
