09e7c77d2837c3a1ef0147c403813b2047931b6b
David Scobie <david.scobie.20@ucl.ac.uk>
2021-01-28 16:57:36 +0000

    completed f

--- week3/week3_problems.py
+++ week3/week3_problems.py
@@ -3,31 +3,31 @@ import matplotlib.pyplot as plt
 import cv2
 
 #a
-a=np.zeros((1,5))
-b=np.ones((1,35))
+n=200
+a=np.zeros((1,round(n/40)))
+b=np.ones((1,round((7*n)/40)))
 c=np.hstack((a,b))
-d=0.2*np.ones((1,40))
+d=0.2*np.ones((1,round((8*n)/40)))
 e=np.hstack((c,d))
-f=-0.5*np.ones((1,40))
+f=-0.5*np.ones((1,round((8*n)/40)))
 g=np.hstack((e,f))
-h=np.zeros((1,20))
+h=np.zeros((1,round((4*n)/40)))
 i=np.hstack((g,h))
-j=0.7*np.ones((1,20))
+j=0.7*np.ones((1,round((4*n)/40)))
 k=np.hstack((i,j))
-l=-0.7*np.ones((1,40))
-f=np.hstack((k,l)).reshape((200, 1))
-x = np.linspace(-1, 1, num=200)
+l=-0.7*np.ones((1,round((8*n)/40)))
+f=np.hstack((k,l)).reshape((n, 1))
+x = np.linspace(-1, 1, num=n)
 
-# plt.figure(0)
+plt.figure(0)
 plt.xlabel("x")
 plt.ylabel("f")
-# plt.plot(x,f)
+plt.plot(x,f)
 
 
 #b
-n=200
 del_n=2/n
-std = 0.2
+std = 0.05
 start = del_n/(((2*np.pi)**0.5)*std)
 A_big=[]
 for i in range (n):
@@ -41,25 +41,45 @@ Aimg = cv2.applyColorMap(Atmp, cv2.COLORMAP_JET)
 cv2.imwrite("Aimage3.png",Aimg)
 
 U, W1, VT = np.linalg.svd(A)
-# plt.plot(W1)
+plt.figure(1)
+plt.title('Singular values of A')
+plt.plot(W1)
 
 print(np.var(W1))
 
 con=np.matmul(A,f)
-plt.figure(1)
+plt.figure(2)
 plt.plot(x,con)
 plt.xlabel("x")
 plt.ylabel("A*f")
 
 Ff = np.fft.fftshift(np.fft.fft(np.fft.fftshift(f)))
-print(Ff.shape)
 FA = np.fft.fftshift(np.fft.fft(np.fft.fftshift(A)))
-print(FA.shape)
 Fcon=FA*Ff
-# Fcon = np.matmul(FA,Ff)
 con2 = np.real(np.fft.fftshift(np.fft.ifft(np.fft.fftshift(Fcon))))
-print(con2.shape)
-plt.figure(2)
+plt.figure(3)
 plt.plot(x,np.sum(con2, axis=0))
+
+#f
+Arep = np.tile(A, (3,3))
+s=np.array(A)
+sarr = np.array(Arep)
+sarr[340:460,140:260] = s[40:160,40:160]
+sarr[140:260,340:460] = s[40:160,40:160]
+# plt.imshow(sarr)
+
+A_bound = sarr[200:400,200:400]
+# plt.imshow(A_bound)
+
+FA_bound = np.fft.fftshift(np.fft.fft(np.fft.fftshift(A_bound)))
+# print(FA.shape)
+Fconbou=FA_bound*Ff
+# Fcon = np.matmul(FA,Ff)
+con2bou = np.real(np.fft.fftshift(np.fft.ifft(np.fft.fftshift(Fconbou))))
+# print(con2.shape)
+plt.figure(3)
+plt.plot(x,np.sum(con2bou, axis=0))
+
+
 
 plt.show()
