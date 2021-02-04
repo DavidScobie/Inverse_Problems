095fa13e01ce3534479fe9d4f994797ef022b7cb
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-04 09:31:30 +0000

    starting part c

--- week4/week4_exercises.py
+++ week4/week4_exercises.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import scipy.ndimage
+from scipy.sparse.linalg import gmres
+from scipy.sparse.linalg import LinearOperator
 
 #a
 img = mpimg.imread('Cameraman256.png')
@@ -14,10 +16,28 @@ plt.colorbar()
 #b
 sigma = 2
 theta = 0.01
-g = scipy.ndimage.filters.gaussian_filter(f_img,sigma)
-w,h = g.shape
-g = g + theta*np.random.randn(w,h)
-plt.figure(1)
-plt.imshow(g)
 
-plt.show()
+# w,h = g.shape
+# g = g + theta*np.random.randn(w,h)
+# plt.figure(1)
+# plt.imshow(g)
+alpha = 0.1
+
+def A(f_img):
+    Af = scipy.ndimage.filters.gaussian_filter(f_img,sigma)
+    return Af
+
+#c
+def ATA(f,alpha):
+    y = A(f)
+    z = A(y) + alpha*f
+    return z
+
+
+A_gmres = scipy.sparse.linalg.LinearOperator((256,256),matvec=ATA)
+
+# gmresOutput = scipy.sparse.linalg.gmres(A_gmres, ATg)
+
+
+
+# plt.show()
