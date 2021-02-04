50e872017a1c1676847a9370e3c7090a7c9b8a2d
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-04 11:27:34 +0000

    completed part c

--- week4/week4_exercises.py
+++ week4/week4_exercises.py
@@ -4,40 +4,41 @@ import matplotlib.image as mpimg
 import scipy.ndimage
 from scipy.sparse.linalg import gmres
 from scipy.sparse.linalg import LinearOperator
+from scipy.ndimage.filters import gaussian_filter
 
 #a
 img = mpimg.imread('Cameraman256.png')
-f_img = np.float32(img)
+f = np.float32(img)
 plt.figure(0)
-imgplot = plt.imshow(f_img,cmap = 'gray')
+imgplot = plt.imshow(f,cmap = 'gray')
 plt.colorbar()
 
 
 #b
-sigma = 2
-theta = 0.01
+sigma = 5
+theta = 0.1
 
-# w,h = g.shape
-# g = g + theta*np.random.randn(w,h)
-# plt.figure(1)
-# plt.imshow(g)
-alpha = 0.1
-
-def A(f_img):
-    Af = scipy.ndimage.filters.gaussian_filter(f_img,sigma)
-    return Af
+g = scipy.ndimage.filters.gaussian_filter(f,sigma)
+w,h = g.shape
+g = g + theta*np.random.randn(w,h)
+plt.figure(1)
+plt.imshow(g,cmap='gray')
 
 #c
-def ATA(f,alpha):
-    y = A(f)
-    z = A(y) + alpha*f
-    return z
+alpha = 0.1
 
+y = lambda f: gaussian_filter(f,sigma)
 
-A_gmres = scipy.sparse.linalg.LinearOperator((256,256),matvec=ATA)
+z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + alpha*f
 
-# gmresOutput = scipy.sparse.linalg.gmres(A_gmres, ATg)
+A = LinearOperator((65536,65536),matvec = z)
 
+b = np.zeros((256**2,1))
 
+ATg = lambda g: gaussian_filter(np.reshape(g,(256,256)),sigma).ravel()
 
-# plt.show()
+gmresOutput = scipy.sparse.linalg.gmres(A,ATg(g), x0 = f.ravel())
+
+plt.figure(2)
+plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
+plt.show()
