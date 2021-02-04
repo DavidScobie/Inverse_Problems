8ad08573dba42e0a10bdb3c5a12d0cf78aab68c3
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-04 15:08:44 +0000

    starting d

--- week4/week4_exercises.py
+++ week4/week4_exercises.py
@@ -5,6 +5,7 @@ import scipy.ndimage
 from scipy.sparse.linalg import gmres
 from scipy.sparse.linalg import LinearOperator
 from scipy.ndimage.filters import gaussian_filter
+from scipy.sparse.linalg import lsqr
 
 #a
 img = mpimg.imread('Cameraman256.png')
@@ -31,9 +32,7 @@ y = lambda f: gaussian_filter(f,sigma)
 
 z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + alpha*f
 
-A = LinearOperator((65536,65536),matvec = z)
-
-b = np.zeros((256**2,1))
+A = LinearOperator((256**2,256**2),matvec = z)
 
 ATg = lambda g: gaussian_filter(np.reshape(g,(256,256)),sigma).ravel()
 
@@ -41,4 +40,26 @@ gmresOutput = scipy.sparse.linalg.gmres(A,ATg(g), x0 = f.ravel())
 
 plt.figure(2)
 plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
-plt.show()
+# plt.show()
+
+#d
+
+print(np.size(A1))
+iden = np.identity(256)
+M_f = lambda M_f:  
+MT_b = lambda MT_b: 
+A1 = LinearOperator((256**2,256**2),matvec = M_f, rmatvec = MT_b)
+# def M_f(A,f):
+#     # implementation of the augmented matrix multiplication
+#     M_f = A*f 
+#     return M_f
+# def MT_b(A,f):
+#     # implementation of the transposed augmented matrix multiplication
+#     MT_b = A.transpose * f
+#     return MT_b
+
+
+
+# siz = g.size
+# b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz,1))])
+# lsqrOutput = scipy.sparse.linalg.lsqr(A,b)
