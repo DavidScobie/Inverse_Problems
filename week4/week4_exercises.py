f0495b986953838472eee6c67e44e9b14392456a
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-09 10:16:56 +0000

    completing week 4 exercises

--- week4/week4_exercises.py
+++ week4/week4_exercises.py
@@ -11,7 +11,6 @@ from scipy.sparse.linalg import lsqr
 #a
 img = mpimg.imread('Cameraman256.png')
 f = np.float32(img)
-print(f.shape)
 plt.figure(0)
 imgplot = plt.imshow(f,cmap = 'gray')
 plt.colorbar()
@@ -23,10 +22,10 @@ theta = 0.01
 
 g = scipy.ndimage.filters.gaussian_filter(f,sigma)
 w,h = g.shape
-print(w,h)
 g = g + theta*np.random.randn(w,h)
 plt.figure(1)
 plt.imshow(g,cmap='gray')
+
 
 #c
 alpha = 0.1
@@ -34,7 +33,6 @@ alpha = 0.1
 y = lambda f: gaussian_filter(f,sigma)
 
 z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + alpha*f
-# z = lambda f: gaussian_filter(y(f),sigma).ravel() + alpha*f
 
 A = LinearOperator((256**2,256**2),matvec = z)
 
@@ -42,70 +40,41 @@ b = np.zeros((256**2,1))
 
 ATg = lambda g: gaussian_filter(np.reshape(g,(256,256)),sigma).ravel()
 
-gmresOutput = gmres(A,ATg(g), x0 = f.ravel())
+class gmres_counter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+    def __call__(self, rk=None):
+        self.niter += 1
+        if self._disp:
+            print('iter %3i\trk = %s' % (self.niter, str(rk)))
 
+counter = gmres_counter()
+
+gmresOutput = gmres(A,ATg(g), x0 = f.ravel(), callback=counter)
+print(counter.niter)
 
 plt.figure(2)
 plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
-# plt.show()
+
 
 #d
-
-top = gaussian_filter(f,sigma).ravel()
-bottom = (alpha**0.5)*np.identity(256).ravel()
-print(np.vstack([top,bottom]))
 
 def M_f(f):
     top = gaussian_filter(f,sigma).ravel()
     bottom = (alpha**0.5)*np.identity(256).ravel()
     return np.vstack([top,bottom])
 
-# def MT_b(f):
-#     unr_l = gaussian_filter(f,sigma).ravel()
-#     left = np.reshape(unr_l,(256**2,1))
-#     unr_r = (alpha**0.5)*np.identity(256).ravel()
-#     right = np.reshape(unr_r,(256**2,1))
-#     return np.hstack((left,right))
-
 def MT_b(b):
-    unr_l = gaussian_filter(np.reshape(b[0:65536],(256,256)),sigma).ravel()
-    # left = np.reshape(unr_l,((256**2)*2,1))
-    return unr_l
-    # unr_r = (alpha**0.5)*np.identity(256*2).ravel()
-    # right = np.reshape(unr_r,((256**2)*2,1))
-    # return np.hstack((left,right))
-
-    
+    return gaussian_filter(np.reshape(b[0:65536],(256,256)),sigma).ravel()
 
 A = LinearOperator(((256**2)*2,256**2),matvec = M_f, rmatvec = MT_b)
-print(A.shape)
 siz = g.size
-print(siz)
 b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz,1))])
-# bottom = (alpha**0.5)*np.identity(256**2)
 lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = f.ravel())
+print(lsqrOutput[2])
 
 plt.figure(3)
 plt.imshow(np.reshape(lsqrOutput[0],(256,256)),cmap='gray')
 plt.show()
 
-# print(np.size(A1))
-
-# M_f = lambda M_f:  
-# MT_b = lambda MT_b: 
-
-# def M_f(f):
-#     #implementation of the augmented matrix multiplication
-#     M_f = np.array([A,(alpha**0.5)*np.identity(256)])
-#     gaussian_filter(f,sigma)
-#     return M_f
-# def MT_b(b):
-#     # implementation of the transposed augmented matrix multiplication
-#     MT_b = np.array([A.transpose,(alpha**0.5)*np.identity(256)])
-#     return MT_b
-# A = LinearOperator((256,256),matvec = M_f, rmatvec = MT_b)
-
-
-# siz = g.size
-# b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz,1))])
-# lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = f.ravel())
