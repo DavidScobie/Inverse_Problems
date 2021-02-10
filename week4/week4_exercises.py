1a18b7c1550a85b010f304461ef08d41ddfae88b
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-10 22:02:46 +0000

    d works

--- week4/week4_exercises.py
+++ week4/week4_exercises.py
@@ -60,21 +60,56 @@ plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
 
 #d
 
+# def M_f(f):
+#     top = gaussian_filter(f,sigma).ravel()
+#     bottom = (alpha**0.5)*np.identity(256).ravel()
+#     return np.vstack([top,bottom])
+
+# def MT_b(b):
+#     b1 = np.reshape(b[0:65536],(256,256))
+#     b2 = np.reshape(b[65536:],(256,256))
+#     return (gaussian_filter(b1,sigma) + np.sqrt(alpha)*b2).ravel()
+#     # return gaussian_filter(np.reshape(b[0:65536],(256,256)),sigma).ravel()
+    
+
+# A = LinearOperator(((256**2)*2,256**2),matvec = M_f, rmatvec = MT_b)
+# siz = g.size
+# b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz,1))])
+# lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = (np.zeros((256,256))).ravel(),atol=1.0e-9)
+# print(lsqrOutput[2])
+
+# plt.figure(3)
+# plt.imshow(np.reshape(lsqrOutput[0],(256,256)),cmap='gray')
+# plt.show()
+
+
+# gau = lambda f: gaussian_filter(np.reshape(f,(256,256)),sigma).ravel()
+
+# A = LinearOperator((65536,65536),matvec = gau, rmatvec = gau)
+
+# lsqrOutput2, dummy, iter_lsqr2 = scipy.sparse.linalg.lsqr(A,g,damp=np.sqrt(alpha),atol=1.0e-9)[:3]
+
+
+g = g.ravel()
+
+gau = lambda f: gaussian_filter(np.reshape(f,(256,256)),sigma).ravel()
+
 def M_f(f):
-    top = gaussian_filter(f,sigma).ravel()
-    bottom = (alpha**0.5)*np.identity(256).ravel()
-    return np.vstack([top,bottom])
+    # implementation of the transposed augmented matrix multiplication
+    return np.append(gau(f),np.sqrt(alpha)*f)
 
-def MT_b(b):
-    return gaussian_filter(np.reshape(b[0:65536],(256,256)),sigma).ravel()
+def MT_f(f):
+    # implementation of the augmented matrix multiplication
+    return (gau(f[:65536]) + np.sqrt(alpha)*f[65536:])
 
-A = LinearOperator(((256**2)*2,256**2),matvec = M_f, rmatvec = MT_b)
-siz = g.size
-b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz,1))])
-lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = f.ravel())
+b = np.append(g,np.zeros_like(g))
+
+A = LinearOperator((131072,65536), matvec = M_f, rmatvec = MT_f)
+
+lsqrOutput, dummy, iter_lsqr = scipy.sparse.linalg.lsqr(A,b,atol=1.0e-9)[:3]
+
 print(lsqrOutput[2])
 
 plt.figure(3)
-plt.imshow(np.reshape(lsqrOutput[0],(256,256)),cmap='gray')
+plt.imshow(np.reshape(lsqrOutput,(256,256)),cmap='gray')
 plt.show()
-
