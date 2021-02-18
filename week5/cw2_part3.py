a2bd113e33ed77d435ded1259c3dcf06b58bb333
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-18 10:55:53 +0000

    I do now get a result for 3b) augmented, but it needs low atol to converge

--- week5/cw2_part3.py
+++ week5/cw2_part3.py
@@ -8,7 +8,7 @@ from scipy.ndimage.filters import gaussian_filter
 from scipy.sparse.linalg import lsqr
 from scipy.sparse import spdiags
 from scipy import sparse
-
+from scipy.sparse import vstack
 
 #a
 img = mpimg.imread('Cameraman256.png')
@@ -48,59 +48,69 @@ A1 = scipy.sparse.identity(256**2)-(0.25*lap)
 
 alpha = 0.1
 
-y = lambda f: gaussian_filter(f,sigma)
+# y = lambda f: gaussian_filter(f,sigma)
 
-z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + np.reshape(alpha*(-lap@sparse.csr_matrix(np.reshape(f,(256**2,1))).toarray()),(1,256**2)).ravel()
+# z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + np.reshape(alpha*(-lap@sparse.csr_matrix(np.reshape(f,(256**2,1))).toarray()),(1,256**2)).ravel()
 
-A = LinearOperator((256**2,256**2),matvec = z)
+# A = LinearOperator((256**2,256**2),matvec = z)
 
-b = np.zeros((256**2,1))
+# b = np.zeros((256**2,1))
 
-ATg = lambda g: gaussian_filter(np.reshape(g,(256,256)),sigma).ravel()
+# ATg = lambda g: gaussian_filter(np.reshape(g,(256,256)),sigma).ravel()
 
-class gmres_counter(object):
-    def __init__(self, disp=True):
-        self._disp = disp
-        self.niter = 0
-    def __call__(self, rk=None):
-        self.niter += 1
-        if self._disp:
-            print('iter %3i\trk = %s' % (self.niter, str(rk)))
+# class gmres_counter(object):
+#     def __init__(self, disp=True):
+#         self._disp = disp
+#         self.niter = 0
+#     def __call__(self, rk=None):
+#         self.niter += 1
+#         if self._disp:
+#             print('iter %3i\trk = %s' % (self.niter, str(rk)))
 
-counter = gmres_counter()
+# counter = gmres_counter()
 
-gmresOutput = gmres(A,ATg(g), x0 = np.zeros((256,256)).ravel(), callback=counter)
-# print(counter.niter)
+# gmresOutput = gmres(A,ATg(g), x0 = np.zeros((256,256)).ravel(), callback=counter)
+# # print(counter.niter)
 
-plt.figure(2)
-plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
+# plt.figure(2)
+# plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
 
 
 #d
+D1x = spdiags(dat,diags_x,256,256)
+D1x2d = sparse.kron(scipy.sparse.identity(256),D1x)
+D1y2d = sparse.kron(D1x,scipy.sparse.identity(256))
+D_2D = vstack([D1x2d,D1y2d])
+D_2D_trans = np.transpose(D_2D)
+print(D_2D_trans)
 
 def M_f(f):
     top = gaussian_filter(f,sigma).ravel()
-    bottom = (alpha**0.5)*-lap@np.reshape(f,(256**2,1)).ravel()
+    middle = (alpha**0.5)*(D1x2d@sparse.csr_matrix(np.reshape(f,(256**2,1)))).toarray().ravel()
+    bottom = (alpha**0.5)*(D1y2d@sparse.csr_matrix(np.reshape(f,(256**2,1)))).toarray().ravel()
     # bottom = np.reshape((alpha**0.5)*(-lap@sparse.csr_matrix(np.reshape(f,(256**2,1))).toarray()),(1,256**2)).ravel()
-    return np.vstack([top,bottom])
+    return np.vstack([top,middle,bottom])
 
 def MT_b(b):
     b1 = np.reshape(b[0:65536],(256,256))
-    b2 = np.reshape(b[65536:],(256,256))
+    Ag = gaussian_filter(b1,sigma)
+
+    b2 = np.reshape(b[65536:],(2*(256**2),1))
+    reg_bit = (alpha**0.5)*np.reshape((D_2D_trans@sparse.csr_matrix(b2)),(256,256)).toarray()
     # return (gaussian_filter(b1,sigma) + (alpha**0.5)*b2).ravel()
-    return (gaussian_filter(b1,sigma) + (alpha**0.5)*-lap.T@b2).ravel()
+    return (Ag + reg_bit).ravel()
     
 
 A = LinearOperator(((256**2)*3,256**2),matvec = M_f, rmatvec = MT_b)
 siz = g.size
 b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz*2,1))])
-lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = np.zeros((256,256)).ravel(),atol=1.0e-9)
+lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = np.zeros((256,256)).ravel(),atol=1.0e-4,iter_lim = 100)
 print(lsqrOutput[2])
 
 plt.figure(3)
 plt.imshow(np.reshape(lsqrOutput[0],(256,256)),cmap='gray')
 
-print(np.reshape(lsqrOutput[0],(256,256))-np.reshape(gmresOutput[0],(256,256)))
+# print(np.reshape(lsqrOutput[0],(256,256))-np.reshape(gmresOutput[0],(256,256)))
 plt.show()
 
 
