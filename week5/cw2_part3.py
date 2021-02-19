5dbe1d49f453973256c572801d73ce9524c23a15
David Scobie <david.scobie.20@ucl.ac.uk>
2021-02-19 09:09:11 +0000

    both methods share the same 256,256 squared D arrays

--- week5/cw2_part3.py
+++ week5/cw2_part3.py
@@ -33,24 +33,28 @@ plt.imshow(g,cmap='gray')
 mid = np.ones([1,257]).flatten()
 dat=np.array([-mid,mid])
 diags_x = np.array([0,-1])
-D1x = spdiags(dat,diags_x,257,256)
+# D1x = spdiags(dat,diags_x,257,256)
+D1x = spdiags(dat,diags_x,256,256)
 
 D1x2d = sparse.kron(scipy.sparse.identity(256),D1x)
 D1y2d = sparse.kron(D1x,scipy.sparse.identity(256))
 
 #try to sort out proper DT_D with syntax later
 # D2d = np.vstack([D1x2d,D1y2d])
-# DT_D = (np.transpose(D2d))@D2d
+D2d = scipy.sparse.vstack([D1x2d,D1y2d])
+D_2D_trans = np.transpose(D2d)
+DT_D = D_2D_trans@D2d
 
-lap = -((np.transpose(D1x2d)@D1x2d) + (np.transpose(D1y2d)@D1y2d))
-A1 = scipy.sparse.identity(256**2)-(0.25*lap)
+# lap = -((np.transpose(D1x2d)@D1x2d) + (np.transpose(D1y2d)@D1y2d))
+# A1 = scipy.sparse.identity(256**2)-(0.25*lap)
 
 
 alpha = 0.1
 
 y = lambda f: gaussian_filter(f,sigma)
 
-z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + np.reshape(alpha*(-lap@sparse.csr_matrix(np.reshape(f,(256**2,1))).toarray()),(1,256**2)).ravel()
+# z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + np.reshape(alpha*(-lap@sparse.csr_matrix(np.reshape(f,(256**2,1))).toarray()),(1,256**2)).ravel()
+z = lambda f: gaussian_filter(y(np.reshape(f,(256,256))),sigma).ravel() + np.reshape(alpha*(DT_D@sparse.csr_matrix(np.reshape(f,(256**2,1))).toarray()),(1,256**2)).ravel()
 
 A = LinearOperator((256**2,256**2),matvec = z)
 
@@ -77,11 +81,11 @@ plt.imshow(np.reshape(gmresOutput[0],(256,256)),cmap='gray')
 
 
 #d
-D1x = spdiags(dat,diags_x,256,256)
-D1x2d = sparse.kron(scipy.sparse.identity(256),D1x)
-D1y2d = sparse.kron(D1x,scipy.sparse.identity(256))
-D_2D = vstack([D1x2d,D1y2d])
-D_2D_trans = np.transpose(D_2D)
+# D1x = spdiags(dat,diags_x,256,256)
+# D1x2d = sparse.kron(scipy.sparse.identity(256),D1x)
+# D1y2d = sparse.kron(D1x,scipy.sparse.identity(256))
+# D_2D = vstack([D1x2d,D1y2d])
+# D_2D_trans = np.transpose(D_2D)
 
 def M_f(f):
     top = gaussian_filter(f,sigma).ravel()
