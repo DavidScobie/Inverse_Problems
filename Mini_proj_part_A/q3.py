65237f4689ab68838064085b2764981f94c66bd5
David Scobie <david.scobie.20@ucl.ac.uk>
2021-03-17 19:53:10 +0000

    labelled figures

--- Mini_proj_part_A/q3.py
+++ Mini_proj_part_A/q3.py
@@ -11,9 +11,11 @@ plt.imshow(f)
 plt.colorbar()
 
 #Add noise to phantom
-theta = 0.01
+# theta = 0.01
+theta = 0.1
 g = f
 w,h = g.shape
+# g = g + theta*np.random.randn(w,h)
 g = g + theta*np.random.randn(w,h)
 plt.figure(1)
 plt.imshow(g)
@@ -33,11 +35,11 @@ projector_id = astra.create_projector('strip', proj_geom, vol_geom)
 SLphantom = f
 sinogram_id, sinogram = astra.create_sino(SLphantom, projector_id,  returnData=True)
 # print(np.shape(sinogram))
-plt.figure(1)
-plt.imshow(np.transpose(sinogram))
-plt.xlabel('angle')
-plt.ylabel('projection sample')
-plt.colorbar()
+# plt.figure(1)
+# plt.imshow(np.transpose(sinogram))
+# plt.xlabel('angle')
+# plt.ylabel('projection sample')
+# plt.colorbar()
 
 projector_id = astra.create_projector('strip', proj_geom, vol_geom)
 rec_id = astra.data2d.create('-vol', vol_geom)
@@ -53,8 +55,6 @@ print(alg_id)
 
 # def A(f,projector_id):
 def A(f):
-    print(f.shape)
-    print(projector_id)
     f_resh = np.reshape(f,(128,128))
     # sinogram_id, sinogram = astra.create_sino(f_resh, projector_id,  returnData=True)
     sinogram_id, sinogram = astra.create_sino(f_resh, 5,  returnData=True)
@@ -63,71 +63,50 @@ def A(f):
 
 # def AT(y,rec_id,alg_id):
 def AT(y):
-    print(y.shape)
     # astra.algorithm.run(alg_id)
     astra.algorithm.run(7)
     # f_rec = astra.data2d.get(rec_id)
     f_rec = astra.data2d.get(6)
     AT_y = f_rec
-    print(rec_id)
-    print(alg_id)
-    print(AT_y.shape)
     return AT_y.ravel()
 
-alpha = 0.1
+alpha = 0.2
 
 # y = AT(A(f,projector_id),rec_id,alg_id) + alpha*f
 
 # z = lambda f: AT(A(f,5),6,7) + 0.1*f
-z = lambda f: AT(A(f)) + 0.1*f
+z = lambda f: AT(A(f)) + alpha*f
 
 A1 = LinearOperator((128**2,128**2),matvec = z)
 
 # ATg = lambda g: AT(g,rec_id,alg_id).ravel()
 ATg = lambda g: AT(g).ravel()
 
-gmresOutput = gmres(A1,ATg(g), x0 = np.zeros((128,128)).ravel(), atol=1e-06)
+class gmres_counter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+    def __call__(self, rk=None):
+        self.niter += 1
+        if self._disp:
+            print('iter %3i\trk = %s' % (self.niter, str(rk)))
+
+counter = gmres_counter()
+
+gmresOutput = gmres(A1,ATg(g), x0 = np.zeros((128,128)).ravel(),callback=counter, atol=1e-06)
 
 plt.figure(2)
-plt.imshow(np.reshape(gmresOutput[0],(128,128)),cmap='gray')
+plt.imshow(np.reshape(gmresOutput[0],(128,128)))
+plt.title('output of gmres')
+plt.colorbar()
 
+astra.algorithm.run(7)
+f_rec = astra.data2d.get(6)
 
+plt.figure(3)
+plt.imshow(f_rec)
+plt.title('FBP reconstruction')
+plt.colorbar()
 
-# def M_f(f):
-#     v,h = f.shape
-#     det_count = 150
-#     angles = np.linspace(0,np.pi,180,endpoint=False)
-#     proj_geom = astra.create_proj_geom('parallel',1.,det_count,angles)
-#     vol_geom = astra.create_vol_geom(v,h)
-#     projector_id = astra.create_projector('strip', proj_geom, vol_geom)
-#     top = astra.create_sino(f, projector_id,  returnData=True)
-
-#     top = gaussian_filter(f,sigma).ravel()
-#     bottom = (alpha**0.5)*np.reshape(f,(256,256)).ravel()
-#     return np.vstack([top,bottom])
-
-# def MT_b(b):
-#     b1 = np.reshape(b[0:128**2],(128,128))
-#     b2 = np.reshape(b[128**2:],(128,128))
-
-#     v,h = b1.shape
-#     vol_geom = astra.create_vol_geom(v,h)
-#     rec_id = astra.data2d.create('-vol', vol_geom)
-#     cfg = astra.astra_dict('FBP')
-#     cfg['ReconstructionDataId'] = rec_id
-#     cfg['ProjectionDataId'] = sinogram_id
-#     cfg['ProjectorId'] = projector_id
-
-#     return (gaussian_filter(b1,sigma) + np.sqrt(alpha)*b2).ravel()
-    
-
-# A = LinearOperator(((128**2)*2,128**2),matvec = M_f, rmatvec = MT_b)
-# siz = g.size
-# b = np.vstack([np.reshape(g,(siz,1)),np.zeros((siz,1))])
-# lsqrOutput = scipy.sparse.linalg.lsqr(A,b, x0 = np.zeros((256,256)).ravel(),atol=1e-06)
-# print(lsqrOutput[2])
-
-# plt.figure(3)
-# plt.imshow(np.reshape(lsqrOutput[0],(256,256)),cmap='gray')
 
 plt.show()
