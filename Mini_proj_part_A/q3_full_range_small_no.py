9b230875194febce00a7646f56cd19d1c7f6df55
David Scobie <david.scobie.20@ucl.ac.uk>
2021-03-18 11:36:19 +0000

    adding limited angles and limited no of angles q3 to repo

--- Mini_proj_part_A/q3_full_range_small_no.py
+++ Mini_proj_part_A/q3_full_range_small_no.py
@@ -7,22 +7,11 @@ from scipy.sparse import spdiags
 from scipy import sparse
 import scipy.ndimage
 
-
-class gmres_counter(object):
-    def __init__(self, disp=True):
-        self._disp = disp
-        self.niter = 0
-    def __call__(self, rk=None):
-        self.niter += 1
-        if self._disp:
-            print('iter %3i\trk = %s' % (self.niter, str(rk)))
-
 #Load phantom in
 f = np.load('SLphan.npy')
 plt.figure(0)
 plt.imshow(f)
 plt.colorbar()
-forig = f
 
 #Add noise to phantom
 # theta = 0.01
@@ -39,7 +28,7 @@ v,h = f.shape
 vol_geom = astra.create_vol_geom(v,h)
 # Create projector geometries
 # angles = np.linspace(0,np.pi,180,endpoint=False)
-angles = np.linspace(0,np.pi,180,endpoint=False)
+angles = np.linspace(0,np.pi,45,endpoint=False)
 det_count = 150
 proj_geom = astra.create_proj_geom('parallel',1.,det_count,angles)
 # Create projector
@@ -68,7 +57,62 @@ plt.title('FBP reconstruction')
 plt.colorbar()
 
 
+#TK0 reg
 # Redefining for the krylov solver
+projector_id = astra.create_projector('strip', proj_geom, vol_geom)
+sinogram_id, sinogram = astra.create_sino(SLphantom, projector_id,  returnData=True)
+rec_id = astra.data2d.create('-vol', vol_geom)
+cfg = astra.astra_dict('FBP')
+cfg['ReconstructionDataId'] = rec_id
+cfg['ProjectionDataId'] = sinogram_id
+cfg['ProjectorId'] = projector_id
+alg_id = astra.algorithm.create(cfg)
+
+print(projector_id)
+print(rec_id)
+print(alg_id)
+
+def A(f):
+    f_resh = np.reshape(f,(128,128))
+    sinogram_id, sinogram = astra.create_sino(f_resh, projector_id,  returnData=True)
+    A_x = sinogram
+    return A_x
+
+def AT(y):
+    astra.algorithm.run(alg_id)
+    f_rec = astra.data2d.get(rec_id)
+    AT_y = f_rec
+    return AT_y.ravel()
+
+alpha = 0.11
+
+z = lambda f: AT(A(f)) + alpha*f
+
+A1 = LinearOperator((128**2,128**2),matvec = z)
+
+ATg = lambda g: AT(g).ravel()
+
+class gmres_counter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+    def __call__(self, rk=None):
+        self.niter += 1
+        if self._disp:
+            print('iter %3i\trk = %s' % (self.niter, str(rk)))
+
+counter = gmres_counter()
+
+gmresOutput = gmres(A1,ATg(g), x0 = np.zeros((128,128)).ravel(),callback=counter, atol=1e-06)
+
+plt.figure(3)
+plt.imshow(np.reshape(gmresOutput[0],(128,128)))
+plt.title('TK0 full range small no. angles')
+plt.colorbar()
+
+
+
+# TK1 reg
 projector_id = astra.create_projector('strip', proj_geom, vol_geom)
 sinogram_id, sinogram = astra.create_sino(SLphantom, projector_id,  returnData=True)
 rec_id = astra.data2d.create('-vol', vol_geom)
@@ -101,56 +145,42 @@ D2d = scipy.sparse.vstack([D1x2d,D1y2d])
 D_2D_trans = sparse.csr_matrix.transpose(scipy.sparse.csr_matrix(D2d))
 DT_D = D_2D_trans@D2d
 
-DP=[]
-rsd_sq=[]
+def A(f):
+    f_resh = np.reshape(f,(128,128))
+    sinogram_id, sinogram = astra.create_sino(f_resh, projector_id,  returnData=True)
+    A_x = sinogram
+    return A_x
 
-powers = np.linspace(-7,-2,num=10)
-alphas = np.exp(powers)
-for alpha in (alphas):
-    print(alpha)
-    diff = []
+def AT(y):
+    astra.algorithm.run(alg_id)
+    f_rec = astra.data2d.get(rec_id)
+    AT_y = f_rec
+    return AT_y.ravel()
 
-    def A(f):
-        f_resh = np.reshape(f,(128,128))
-        sinogram_id, sinogram = astra.create_sino(f_resh, projector_id,  returnData=True)
-        A_x = sinogram
-        return A_x
+alpha = 0.0015
 
-    def AT(y):
-        astra.algorithm.run(alg_id)
-        f_rec = astra.data2d.get(rec_id)
-        AT_y = f_rec
-        return AT_y.ravel()
+z = lambda f: AT(A(f)) + + (alpha*(DT_D@sparse.csr_matrix(np.reshape(f,(128**2,1))).toarray())).ravel()
 
-    z = lambda f: AT(A(f)) + (alpha*(DT_D@sparse.csr_matrix(np.reshape(f,(128**2,1))).toarray())).ravel()
+A1 = LinearOperator((128**2,128**2),matvec = z)
 
-    A1 = LinearOperator((128**2,128**2),matvec = z)
+ATg = lambda g: AT(g).ravel()
 
-    ATg = lambda g: AT(g).ravel()
+class gmres_counter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+    def __call__(self, rk=None):
+        self.niter += 1
+        if self._disp:
+            print('iter %3i\trk = %s' % (self.niter, str(rk)))
 
-    counter = gmres_counter()
+counter = gmres_counter()
 
-    gmresOutput = gmres(A1,ATg(g), x0 = np.zeros((128,128)).ravel(),callback=counter, atol=1e-6)
-
-    long_f = forig.ravel()
-    long_gmres = gmresOutput[0].ravel()
-
-    for i in range (128**2):
-        diff.append(np.abs(long_f[i]-long_gmres[i]))
-
-    residual = np.sum(diff)
-    rsd_sq.append(residual**2)   
-    DP.append(((residual**2)/(256**2))-(theta**2))
-
-plt.figure(3)
-plt.imshow(np.reshape(gmresOutput[0],(128,128)))
-plt.title('output of gmres')
-plt.colorbar()
+gmresOutput = gmres(A1,ATg(g), x0 = np.zeros((128,128)).ravel(),callback=counter, atol=1e-06)
 
 plt.figure(4)
-plt.plot(alphas,DP)
-plt.xlabel('Alpha')
-plt.ylabel('Discrepancy Principle')
-plt.title('Krylov solver Discrepancy Principle')
+plt.imshow(np.reshape(gmresOutput[0],(128,128)))
+plt.title('TK1 full range small no. angles')
+plt.colorbar()
 
 plt.show()
