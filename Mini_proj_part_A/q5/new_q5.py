eee54c86a9a36cddfaebc65dcee62f76d1697747
David Scobie <david.scobie.20@ucl.ac.uk>
2021-03-28 18:35:20 +0100

    wavelet included and threshold mu times lambda working

--- Mini_proj_part_A/q5/new_q5.py
+++ Mini_proj_part_A/q5/new_q5.py
@@ -49,22 +49,37 @@ AT_Af_min_g = astra.data2d.get(rec_id)
 # plt.imshow(AT_Af_min_g)
 
 #define lambda
-lambd = 0.000001
+lambd = 0.00005
+mu = 20
 
 f_min_all_sorts = f - lambd*AT_Af_min_g
 plt.figure(4)
 plt.imshow(f_min_all_sorts)
 
+def thresholdFunction(coeffs,tRange,tVal):
+    bit = []
+    arr,coeff_slices = pywt.coeffs_to_array(coeffs)
+    for i in range (tRange):
+        small_bit = coeffs[i]
+        bit.append(np.shape(small_bit)[1])
+    new_arr = arr
+    for i in range (np.sum(bit)):
+        for j in range (np.sum(bit)):
+            new_arr[i][j] = pywt.threshold(np.array([arr[i][j]]),tVal,'soft')
+    coeffsT = pywt.array_to_coeffs(new_arr, coeff_slices, output_format='wavedec2')
+    return coeffsT
+
 #loop through all this
 def iterative(f):
     #first we do sinogram of f
-    sinogram_id, sinogram = astra.create_sino(f, projector_id,  returnData=True)
+    sinogram_id, Afk = astra.create_sino(f, projector_id,  returnData=True)
 
     #then we add noise to it to make g
-    g = astra.functions.add_noise_to_sino(sinogram,100)
+    # g = astra.functions.add_noise_to_sino(sinogram,100)
+    sinogram_id, g = astra.create_sino(forig, projector_id,  returnData=True)
 
     #take these away from each other
-    Af_min_g = sinogram - g
+    Af_min_g = Afk - g
 
     #Set up params for AT of Af_min_g
     Af_min_g_id = astra.data2d.create('-sino',proj_geom,Af_min_g)
@@ -80,7 +95,16 @@ def iterative(f):
     #f minus all sorts
     f_min_all_sorts = f - lambd*AT_Af_min_g
 
-    return f_min_all_sorts
+    #wavelet decomposition
+    wave = pywt.wavedec2(f_min_all_sorts,'haar',level = 6)
+    #threshold function
+    Essed = thresholdFunction(wave,2,mu*lambd)
+    #Essed = wave
+    #wavelet recomposition
+    inv_wave = pywt.waverec2(Essed,'haar')
+
+    return inv_wave
+    # return f_min_all_sorts
 
 theta = 0.01
 sigma = 2
@@ -90,7 +114,7 @@ plt.figure(0)
 plt.imshow(f)
 print(np.sum(abs(forig - f)))
 
-for i in range(30):
+for i in range(70):
     # print(i)
     f = iterative(f)
     print(np.sum(abs(forig - f)))
