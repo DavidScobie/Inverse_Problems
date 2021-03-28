29233819dafd8668090c4edc6bd7f6aa95607a8e
David Scobie <david.scobie.20@ucl.ac.uk>
2021-03-28 19:06:44 +0100

    works pretty well

--- Mini_proj_part_A/q5/new_q5.py
+++ Mini_proj_part_A/q5/new_q5.py
@@ -53,8 +53,8 @@ lambd = 0.00005
 mu = 20
 
 f_min_all_sorts = f - lambd*AT_Af_min_g
-plt.figure(4)
-plt.imshow(f_min_all_sorts)
+# plt.figure(4)
+# plt.imshow(f_min_all_sorts)
 
 def thresholdFunction(coeffs,tRange,tVal):
     bit = []
@@ -75,8 +75,8 @@ def iterative(f):
     sinogram_id, Afk = astra.create_sino(f, projector_id,  returnData=True)
 
     #then we add noise to it to make g
-    # g = astra.functions.add_noise_to_sino(sinogram,100)
-    sinogram_id, g = astra.create_sino(forig, projector_id,  returnData=True)
+    g = astra.functions.add_noise_to_sino(sinogram,100)
+    # sinogram_id, g = astra.create_sino(forig, projector_id,  returnData=True)
 
     #take these away from each other
     Af_min_g = Afk - g
@@ -106,22 +106,48 @@ def iterative(f):
     return inv_wave
     # return f_min_all_sorts
 
-theta = 0.01
-sigma = 2
-f = gaussian_filter(f,sigma) + theta*np.random.randn(w,h)
+# theta = 0.01
+# sigma = 2
+# f = gaussian_filter(f,sigma) + theta*np.random.randn(w,h)
+f = np.ones([128,128])
 
 plt.figure(0)
-plt.imshow(f)
+plt.imshow(forig)
+plt.colorbar()
 print(np.sum(abs(forig - f)))
 
-for i in range(70):
-    # print(i)
+fbef = f
+i=0
+while np.sum(abs(forig - fbef)) >= np.sum(abs(forig - f)) and i <= 50:
+# for i in range(70):
+    fbef = f
+    i=i+1
     f = iterative(f)
     print(np.sum(abs(forig - f)))
     # plt.figure(i)
     # plt.imshow(f)
 
+#checking what the noisy g actually looks like
+gNoisy_id = astra.data2d.create('-sino',proj_geom,sinogram+g)
+rec_id = astra.data2d.create('-vol', vol_geom)
+# Set up the parameters for a reconstruction via back-projection
+cfg = astra.astra_dict('FBP')
+cfg['ReconstructionDataId'] = rec_id
+cfg['ProjectionDataId'] = gNoisy_id
+cfg['ProjectorId'] = projector_id
+# Create the algorithm object from the configuration structure
+alg_id = astra.algorithm.create(cfg)
+# Run back-projection and get the reconstruction
+astra.algorithm.run(alg_id)
+f_rec = astra.data2d.get(rec_id)
 plt.figure(1)
+plt.imshow(f_rec)
+plt.colorbar()
+
+
+print(np.sum(abs(forig - f_rec)))
+plt.figure(2)
 plt.imshow(f)
+plt.colorbar()
 
 plt.show()
